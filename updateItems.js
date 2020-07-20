19ed1ea3b9d270c11116f4a6063a3a9bdcd1db1f
Ruben <Ruben@MBP-van-Ruben-2.domain_not_set.invalid>
2020-07-20 11:37:15 +0200

    fix: Only update items that are newer than the oldest saved item

--- updateItems.js
+++ updateItems.js
@@ -1,4 +1,5 @@
 const queryHackerNews = require('./helpers/queryHackerNews')
+const fetchOldestSavedItem = require('./helpers/fetchOldestSavedItem')
 const upsertUser = require('./helpers/upsertUser')
 const upsertItem = require('./helpers/upsertItem')
 
@@ -13,8 +14,11 @@ module.exports = async () => {
   const updates = await queryHackerNews('updates')
   if (!updates || JSON.stringify(updates) === previousUpdates) return
 
+  const oldestSavedItem = await fetchOldestSavedItem()
+  const relevantItems = updates.items.filter(itemId => itemId >= oldestSavedItem)
+
   await Promise.all(updates.profiles.map(upsertUser))
-  await Promise.all(updates.items.map(upsertItem))
+  await Promise.all(relevantItems.map(upsertItem))
 
   previousUpdates = JSON.stringify(updates)
   console.log(`Updated ${updates.items.length} items and ${updates.profiles.length} users`)
