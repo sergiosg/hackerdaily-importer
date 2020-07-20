43f3302004e575637459197e5048a6b77913361b
Ruben <Ruben@MBP-van-Ruben-2.domain_not_set.invalid>
2020-07-20 10:56:07 +0200

    refactor: Move the item upsert functionality to its own function

--- importNewItems.js
+++ importNewItems.js
@@ -1,8 +1,5 @@
 const fetchItemsToFetch = require('./helpers/fetchItemsToFetch')
-const queryHackerNews = require('./helpers/queryHackerNews')
-const upsertUser = require('./helpers/upsertUser')
-const upsertStory = require('./helpers/upsertStory')
-const upsertComment = require('./helpers/upsertComment')
+const upsertItem = require('./helpers/upsertItem')
 
 /**
   * fetchNewItems - Fetch all items that have been added since the last update
@@ -16,20 +13,12 @@ module.exports = async () => {
 
   // Loop over all new item IDs
   for (const itemId of itemsToFetch) {
-    // Fetch the item and skip if the item is null
-    const item = await queryHackerNews(`item/${itemId}`)
-    if (item === null) continue
-
-    // Create the user if it doesn't exist yet
-    if (item.by) await upsertUser(item.by)
-
-    // Create the story or comment
-    if (item.type === 'story') upsertStory(item)
-    else if (item.type === 'comment') upsertComment(item)
+    const item = await upsertItem(itemId)
 
     // Update to the console
+    const itemType = item ? item.type : 'null'
     const percentageDone = ((itemId - itemsToFetch[0]) / itemsToFetch.length * 100).toFixed(3)
-    console.log(`${item.type.padEnd(7)} - ${item.id} (${percentageDone}%)`)
+    console.log(`${itemType.padEnd(7)} - ${item.id} (${percentageDone}%)`)
   }
   console.log('Finished!')
 }
