aa5f617fb1074a80b000954e14a4553a90a20570
Ruben <Ruben@MBP-van-Ruben-2.domain_not_set.invalid>
2020-07-20 11:40:13 +0200

    fix: Import all items that are newer than the oldest item and are not yet in the database

--- helpers/fetchItemsToFetch.js
+++ helpers/fetchItemsToFetch.js
@@ -1,23 +1,15 @@
 const queryHackerNews = require('./queryHackerNews')
 const queryHackerDaily = require('./queryHackerDaily')
 
-const maxStoryAndCommentQuery = `
-  {
-    stories_aggregate {
-      aggregate {
-        max {
-          id
-        }
-      }
-    }
-    comments_aggregate {
-      aggregate {
-        max {
-          id
-        }
-      }
-    }
+const allStoryAndCommentIds = `
+{
+  stories {
+    id
   }
+  comments {
+    id
+  }
+}
 `
 
 /**
@@ -32,13 +24,15 @@ module.exports = async () => {
     throw new Error('Max item on HN is not defined')
   }
 
-  const response = await queryHackerDaily(maxStoryAndCommentQuery)
+  const { stories, comments } = await queryHackerDaily(allStoryAndCommentIds)
+  const items = [...stories.map(({ id }) => id), ...comments.map(({ id }) => id)].sort()
 
-  const maxStory = response.stories_aggregate.aggregate.max.id
-  const maxComment = response.comments_aggregate.aggregate.max.id
+  const minExistingItem = items[0] || maxNewItem - 20000
 
-  const maxSavedItem = Math.max(maxStory, maxComment) || maxNewItem - 10000
+  const itemsToFetch = []
+  for (let i = minExistingItem; i <= maxNewItem; i++) {
+    if (!items.includes(i)) itemsToFetch.push(i)
+  }
 
-  // Return an array with all item IDs that need to be fetched
-  return Array(maxNewItem - maxSavedItem).fill().map((_, index) => maxSavedItem + index + 1)
+  return itemsToFetch
 }
