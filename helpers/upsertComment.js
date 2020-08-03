cf774f147e6287a53ddfd4a128983b5d915e46f9
Ruben <>
2020-08-04 00:54:55 +0200

    feat: Add the sentiment of a comment

--- helpers/upsertComment.js
+++ helpers/upsertComment.js
@@ -1,14 +1,38 @@
+const Sentiment = require('sentiment')
 const queryHackerDaily = require('./queryHackerDaily')
 const fetchAscendants = require('./fetchAscendants')
 const unixToIsoString = require('./unixToIsoString')
 
+var sentiment = new Sentiment()
+
 const query = `
   mutation ($comment: comments_insert_input!) {
-    insert_comment(object: $comment, on_conflict: {constraint: comments_pkey, update_columns: [text, user_id, story_id, parent_comment_id, ]}) {
+    insert_comment(object: $comment, on_conflict: {constraint: comments_pkey, update_columns: [text, user_id, story_id, parent_comment_id, sentiment]}) {
       id
     }
   }
 `
+
+/**
+ * measureSentiment - Estimate the amount of positivity in a comment
+ *
+ * @param {String} text The text of the comment
+ *
+ * @return {Number} The amount of positivite sentiment
+ */
+const measureSentiment = (text) => {
+  if (!text) return null
+
+  // Remove quotes since they're not written by the commenter
+  const cleanedText = text
+    .split('<p>')
+    .filter(sentence => !sentence.startsWith('&gt;'))
+    .join(' ')
+    .replace(/&#x27;/g, '\'')
+
+  const { comparative } = sentiment.analyze(cleanedText)
+  return Math.min(Math.max(comparative + 1, 2, 0.5), 1.5)
+}
 
 /**
   * upsertComment - Create or update a comment in the HackerDaily back-end
@@ -26,6 +50,7 @@ module.exports = async ({ id, by, text, parent, time, deleted = false, dead = fa
     text,
     deleted,
     dead,
+    sentiment: measureSentiment(text),
     user_id: by,
     story_id: story,
     parent_comment_id: parentComment,
